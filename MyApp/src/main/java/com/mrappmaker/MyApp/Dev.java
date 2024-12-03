7f7828dcf0cb54dd934ca8d0f8e2915680326b2c
rane254 <mr.appmaker.dev@gmail.com>
2024-12-03 19:53:57 +0530

    Connecting a class using @Autowired

--- MyApp/src/main/java/com/mrappmaker/MyApp/Dev.java
+++ MyApp/src/main/java/com/mrappmaker/MyApp/Dev.java
@@ -1,10 +1,16 @@
 package com.mrappmaker.MyApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+
+    @Autowired // it connects both classes without requiring to create an instance
+    private Laptop laptop;
+
     public void build() {
+        laptop.compile();
         System.out.println("Building an awesome spring project!");
     }
 }
